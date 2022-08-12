6ce02cee265d0a7af0f8a0fe8b32de2564fa231d
Bekzod <bekzod.k2407@gmail.com>
2022-08-12 19:58:28 +0500

    added userDefaults and navbarItem

--- Todoey/MainViewController.swift
+++ Todoey/MainViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    let items: [String] = ["Kim","Jon", "Bon" , "Xon"]
-    
+    var todoItems: [String] = ["A","A","B"]
+    var defaults = UserDefaults.standard
     private lazy var tavleView: UITableView = {
         var view = UITableView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -20,38 +20,70 @@ class MainViewController: UIViewController {
         view.register(TodoCell.self, forCellReuseIdentifier: TodoCell.cellName)
         return view
     }()
+    private lazy var alertTextField: UITextField = {
+        var view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubView()
-        navigationController?.navigationBar.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-    }
+        createBarButtonItems()
+        if let items  = defaults.array(forKey: "TodoItems") as? [String] {
+            todoItems = items
+        }
+     }
     private func setupSubView() {
+        navigationItem.title = "TODO APP"
         view.addSubview(tavleView)
-        
         NSLayoutConstraint.activate([
             tavleView.topAnchor.constraint(equalTo: view.topAnchor),
             tavleView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tavleView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tavleView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-
         ])
     }
+    private func createBarButtonItems() {
+
+        let barItem: UIBarButtonItem = {
+            let view = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
+            return view
+        }()
+        navigationItem.setRightBarButton(barItem, animated: true)
+    }
+    @objc func addButtonPressed() {
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item", style: .default) { action in
+            guard let text = self.alertTextField.text else {return}
+            self.todoItems.append(text)
+            self.defaults.set(self.todoItems, forKey: "TodoItems")
+            self.tavleView.reloadData()
+        }
+        alert.addTextField { textField in
+            textField.placeholder = "Create a new item"
+            self.alertTextField = textField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return todoItems.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TodoCell.cellName) as? TodoCell else {return UITableViewCell()}
-        cell.nameLabel.text = items[indexPath.row]
+        cell.nameLabel.text = todoItems[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
+        
         tableView.deselectRow(at: indexPath, animated: true)
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
