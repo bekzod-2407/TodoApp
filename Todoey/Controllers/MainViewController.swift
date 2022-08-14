515be955094c12eff9a513dba72830193137f4e9
Bekzod <bekzod.k2407@gmail.com>
2022-08-14 21:52:41 +0500

    changed some functionality added searchBar

--- Todoey/Controllers/MainViewController.swift
+++ Todoey/Controllers/MainViewController.swift
@@ -11,9 +11,8 @@ import CoreData
 class MainViewController: UIViewController {
     
     var todoItems = [TodoItem]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.Plist")
     let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-   
+    
     private lazy var tavleView: UITableView = {
         var view = UITableView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -27,30 +26,44 @@ class MainViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    private lazy var searchBar: UISearchBar = {
+        var view = UISearchBar()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.placeholder = "Search"
+        view.frame = CGRect(x: 0, y: 0, width: (navigationController?.view.bounds.size.width)!, height: 64)
+        view.barStyle = .default
+        view.isTranslucent = false
+        return view
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupSubView()
         createBarButtonItems()
         // user’s home directory // append the given path
-        
-        print(dataFilePath)
-//        loadItems()
-     }
+        loadItems()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        searchBar.delegate = self
+    }
     private func setupSubView() {
-       
+        navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         navigationItem.title = "TODO APP"
         view.addSubview(tavleView)
+        view.addSubview(searchBar)
+        let viewsHeight =  navigationController!.navigationBar.bottomAnchor
         NSLayoutConstraint.activate([
-            tavleView.topAnchor.constraint(equalTo: view.topAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor ),
+            tavleView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             tavleView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tavleView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tavleView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
     private func createBarButtonItems() {
-
         let barItem: UIBarButtonItem = {
             let view = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonPressed))
             return view
@@ -61,15 +74,17 @@ class MainViewController: UIViewController {
         
         let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { [self] action in
-          
+            
             let newItem = TodoItem(context: contex)
             guard let text = self.alertTextField.text else {return}
             newItem.title = text
             newItem.isDone =  false
             todoItems.append(newItem)
+            
             saveItem()
-          
+            
         }
+        
         alert.addTextField { textField in
             textField.placeholder = "Create a new item"
             self.alertTextField = textField
@@ -79,7 +94,7 @@ class MainViewController: UIViewController {
         present(alert, animated: true, completion: nil)
         
     }
-   private func saveItem() {
+    private func saveItem() {
         do {
             try contex.save()
         } catch {
@@ -87,18 +102,19 @@ class MainViewController: UIViewController {
         }
         self.tavleView.reloadData()
     }
-//    private func loadItems() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//                todoItems = try decoder.decode([TodoItem].self, from: data)
-//            }catch {
-//                print("cannot decode TodoItem \(error.localizedDescription)")
-//            }
-//        }
-//    }
+    
+    private func loadItems(with request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest() ) {
+        do {
+            todoItems =  try contex.fetch(request)
+        }catch {
+            print("Hello")
+        }
+        tavleView.reloadData()
+    }
 }
 
+
+//MARK: - TableViewDelegate Methods
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoItems.count
@@ -109,15 +125,37 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         let item = todoItems[indexPath.row]
         cell.nameLabel.text = item.title
         cell.accessoryType = item.isDone ? .checkmark : .none
-         
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-            todoItems[indexPath.row].isDone = !todoItems[indexPath.row].isDone
+//                contex.delete(todoItems[indexPath.row])
+//                todoItems.remove(at: indexPath.row)
+        todoItems[indexPath.row].isDone = !todoItems[indexPath.row].isDone
         saveItem()
         tableView.deselectRow(at: indexPath, animated: true)
- 
+    }
+}
+
+
+//MARK: - searchBarDelegate Methods
+extension MainViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<TodoItem> = TodoItem.fetchRequest()
+        guard let text = searchBar.text else {return}
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", text)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
     }
 }
 
--- Todoey/SupportingFiles/AppDelegate.swift
+++ Todoey/SupportingFiles/AppDelegate.swift
@@ -20,13 +20,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         let controller =  MainViewController()
         window?.rootViewController = UINavigationController(rootViewController: controller)
+//        window?.backgroundColor = .white
+        window?.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+
         return true
     }
     
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
-    }
+   
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = NSPersistentContainer(name: "DataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                 
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
@@ -49,7 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             do {
                 try context.save()
             } catch {
-                 
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
